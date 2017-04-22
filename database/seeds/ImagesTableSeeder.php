9209fa5672e8609812c674235962469e01c4c997
Angel Roberto Ruiz Mendoza <Angel Roberto Ruiz Mendoza>
2017-04-22 14:06:26 -0500

    Seeds de imagenes

--- database/seeds/ImagesTableSeeder.php
+++ database/seeds/ImagesTableSeeder.php
@@ -11,10 +11,100 @@ class ImagesTableSeeder extends Seeder
      */
     public function run()
     {
-         DB::table('species')->insert([
+         DB::table('images')->insert([
         	'name'=>'rosaCalifornicaPrincipal',
-            'path' =>'img/species/',
+            'path' =>'img/species/rosaCalifornicaPrincipal.jpg',
             'specie_id' =>1,
+        ]);
+
+        DB::table('images')->insert([
+        	'name'=>'rosaCalifornica1',
+            'path' =>'img/species/rosaCalifornica1.jpg',
+            'specie_id' =>1,
+        ]);
+
+        DB::table('images')->insert([
+        	'name'=>'rosaCalifornica2',
+            'path' =>'img/species/rosaCalifornica2.jpg',
+            'specie_id' =>1,
+        ]);
+
+        DB::table('images')->insert([
+        	'name'=>'rosaCalifornica2',
+            'path' =>'img/species/rosaCalifornica3.jpg',
+            'specie_id' =>1,
+        ]);
+
+        DB::table('images')->insert([
+        	'name'=>'rosaBracteataPrincipal',
+            'path' =>'img/species/rosaBracteataPrincipal.jpg',
+            'specie_id' =>2,
+        ]);
+
+        DB::table('images')->insert([
+        	'name'=>'rosaBracteata1',
+            'path' =>'img/species/rosaBracteata1.jpg',
+            'specie_id' =>2,
+        ]);
+
+        DB::table('images')->insert([
+        	'name'=>'rosaBracteata2',
+            'path' =>'img/species/rosaBracteata2.jpg',
+            'specie_id' =>2,
+        ]);
+
+        DB::table('images')->insert([
+        	'name'=>'rosaBracteata3',
+            'path' =>'img/species/rosaBracteata3.jpg',
+            'specie_id' =>2,
+        ]);
+
+        DB::table('images')->insert([
+        	'name'=>'eichhorniaAzureaPrincipal',
+            'path' =>'img/species/eichhorniaAzureaPrincipal.jpg',
+            'specie_id' =>3,
+        ]);
+
+        DB::table('images')->insert([
+        	'name'=>'eichhorniaAzurea1',
+            'path' =>'img/species/eichhorniaAzurea1.jpg',
+            'specie_id' =>3,
+        ]);
+
+        DB::table('images')->insert([
+        	'name'=>'eichhorniaAzurea2',
+            'path' =>'img/species/eichhorniaAzurea2.jpg',
+            'specie_id' =>3,
+        ]);
+
+         DB::table('images')->insert([
+        	'name'=>'eichhorniaAzurea3',
+            'path' =>'img/species/eichhorniaAzurea3.jpg',
+            'specie_id' =>3,
+        ]);
+
+         DB::table('images')->insert([
+        	'name'=>'eichhorniaCrassipesPrincipal',
+            'path' =>'img/species/eichhorniaCrassipesPrincipal.jpg',
+            'specie_id' =>4,
+        ]);
+
+        DB::table('images')->insert([
+        	'name'=>'eichhorniaCrassipes1',
+            'path' =>'img/species/eichhorniaCrassipes1.jpg',
+            'specie_id' =>4,
+        ]);
+
+        DB::table('images')->insert([
+        	'name'=>'eichhorniaCrassipes2',
+            'path' =>'img/species/eichhorniaCrassipes2.jpg',
+            'specie_id' =>4,
+        ]);
+
+        DB::table('images')->insert([
+        	'name'=>'eichhorniaCrassipes3',
+            'path' =>'img/species/eichhorniaCrassipes3.jpg',
+            'specie_id' =>4,
         ]);
     }
 }
