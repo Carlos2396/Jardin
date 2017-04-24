29c509b3fef8d93273c0fa7513d67555032ae39f
Angel Roberto Ruiz Mendoza <angel.r.ruiz.m@gmail.com>
2017-04-24 11:55:09 -0500

    Se modifico la seeder de images

--- database/seeds/ImagesTableSeeder.php
+++ database/seeds/ImagesTableSeeder.php
@@ -13,97 +13,97 @@ class ImagesTableSeeder extends Seeder
     {
          DB::table('images')->insert([
         	'name'=>'rosaCalifornicaPrincipal',
-            'path' =>'img/species/rosaCalifornicaPrincipal.jpg',
+            'path' =>'img/species/rosaCalifornica_0jpg',
             'specie_id' =>1,
         ]);
 
         DB::table('images')->insert([
         	'name'=>'rosaCalifornica1',
-            'path' =>'img/species/rosaCalifornica1.jpg',
+            'path' =>'img/species/rosaCalifornica_1.jpg',
             'specie_id' =>1,
         ]);
 
         DB::table('images')->insert([
         	'name'=>'rosaCalifornica2',
-            'path' =>'img/species/rosaCalifornica2.jpg',
+            'path' =>'img/species/rosaCalifornica_2.jpg',
             'specie_id' =>1,
         ]);
 
         DB::table('images')->insert([
         	'name'=>'rosaCalifornica2',
-            'path' =>'img/species/rosaCalifornica3.jpg',
+            'path' =>'img/species/rosaCalifornica_3.jpg',
             'specie_id' =>1,
         ]);
 
         DB::table('images')->insert([
         	'name'=>'rosaBracteataPrincipal',
-            'path' =>'img/species/rosaBracteataPrincipal.jpg',
+            'path' =>'img/species/rosaBracteata_0.jpg',
             'specie_id' =>2,
         ]);
 
         DB::table('images')->insert([
         	'name'=>'rosaBracteata1',
-            'path' =>'img/species/rosaBracteata1.jpg',
+            'path' =>'img/species/rosaBracteata_1.jpg',
             'specie_id' =>2,
         ]);
 
         DB::table('images')->insert([
         	'name'=>'rosaBracteata2',
-            'path' =>'img/species/rosaBracteata2.jpg',
+            'path' =>'img/species/rosaBracteata_2.jpg',
             'specie_id' =>2,
         ]);
 
         DB::table('images')->insert([
         	'name'=>'rosaBracteata3',
-            'path' =>'img/species/rosaBracteata3.jpg',
+            'path' =>'img/species/rosaBracteata_3.jpg',
             'specie_id' =>2,
         ]);
 
         DB::table('images')->insert([
         	'name'=>'eichhorniaAzureaPrincipal',
-            'path' =>'img/species/eichhorniaAzureaPrincipal.jpg',
+            'path' =>'img/species/eichhorniaAzurea_0.jpg',
             'specie_id' =>3,
         ]);
 
         DB::table('images')->insert([
         	'name'=>'eichhorniaAzurea1',
-            'path' =>'img/species/eichhorniaAzurea1.jpg',
+            'path' =>'img/species/eichhorniaAzurea_1.jpg',
             'specie_id' =>3,
         ]);
 
         DB::table('images')->insert([
         	'name'=>'eichhorniaAzurea2',
-            'path' =>'img/species/eichhorniaAzurea2.jpg',
+            'path' =>'img/species/eichhorniaAzurea_2.jpg',
             'specie_id' =>3,
         ]);
 
          DB::table('images')->insert([
         	'name'=>'eichhorniaAzurea3',
-            'path' =>'img/species/eichhorniaAzurea3.jpg',
+            'path' =>'img/species/eichhorniaAzurea_3.jpg',
             'specie_id' =>3,
         ]);
 
          DB::table('images')->insert([
         	'name'=>'eichhorniaCrassipesPrincipal',
-            'path' =>'img/species/eichhorniaCrassipesPrincipal.jpg',
+            'path' =>'img/species/eichhorniaCrassipes_0.jpg',
             'specie_id' =>4,
         ]);
 
         DB::table('images')->insert([
         	'name'=>'eichhorniaCrassipes1',
-            'path' =>'img/species/eichhorniaCrassipes1.jpg',
+            'path' =>'img/species/eichhorniaCrassipes_1.jpg',
             'specie_id' =>4,
         ]);
 
         DB::table('images')->insert([
         	'name'=>'eichhorniaCrassipes2',
-            'path' =>'img/species/eichhorniaCrassipes2.jpg',
+            'path' =>'img/species/eichhorniaCrassipes_2.jpg',
             'specie_id' =>4,
         ]);
 
         DB::table('images')->insert([
         	'name'=>'eichhorniaCrassipes3',
-            'path' =>'img/species/eichhorniaCrassipes3.jpg',
+            'path' =>'img/species/eichhorniaCrassipes_3.jpg',
             'specie_id' =>4,
         ]);
     }
